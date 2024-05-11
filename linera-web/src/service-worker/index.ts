b614facff313b7863858e9c6e6dbf69419087bc0
James Kay <james.kay@linera.io>
2024-05-11 05:34:35 +0100

    Saving and loading wallet to local storage

--- linera-web/src/service-worker/index.ts
+++ linera-web/src/service-worker/index.ts
@@ -1,10 +1,1 @@
-chrome.sidePanel.setPanelBehavior({
-  openPanelOnActionClick: true,
-}).catch((error) => console.error(error));
-
-chrome.runtime.onInstalled.addListener(() => (async () => {
-  const windowId = (await chrome.windows.getCurrent()).id;
-  if (windowId === undefined) return;
-  chrome.action.setPopup({ popup: "src/welcome/index.html" });
-  await chrome.action.openPopup({ windowId });
-})());
+import './setup.ts';
