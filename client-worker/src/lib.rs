89db1be1459d9d28a2d0b7acafb62e97e96f6b1a
James Kay <james.kay@linera.io>
2024-08-14 15:33:40 +0100

    `client-worker`: update to `linera-protocol` with a client library

--- client-worker/src/lib.rs
+++ client-worker/src/lib.rs
@@ -9,38 +9,17 @@ connected_.  Outside of their type, which is checked at call time,
 arguments to these functions cannot be trusted and _must_ be verified!
 */
 
-use linera_base::{
-    crypto::{CryptoRng, CryptoHash, KeyPair, PublicKey},
-    data_types::{Amount, BlockHeight, Timestamp},
-    identifiers::{
-        ChainDescription,
-        ChainId,
-    },
+use linera_core::node::{
+    LocalValidatorNode as _,
+    LocalValidatorNodeProvider as _,
 };
-use linera_core::{
-    client::{ArcChainClient, ChainClient, ChainClientBuilder},
-    node::{
-        LocalValidatorNode as _,
-        LocalValidatorNodeProvider as _,
-    },
-};
-use linera_rpc::node_provider::{NodeOptions, NodeProvider};
 
 use wasm_bindgen::prelude::*;
 use web_sys::*;
 
-use std::collections::{BTreeMap, HashMap};
 use std::sync::RwLock;
-use std::time::Duration;
 
-mod config;
-mod options;
-mod wallet;
-
-use options::ClientOptions;
-use wallet::Wallet;
-
-type ChainClients<P, S> = std::collections::BTreeMap<ChainId, ArcChainClient<P, S>>;
+use linera_client::{chain_listener::ClientContext as _, client_options::ClientOptions, wallet::Wallet};
 
 // TODO convert to IndexedDbStore once we refactor Context
 type WebStorage = linera_storage::DbStorage<
@@ -48,7 +27,7 @@ type WebStorage = linera_storage::DbStorage<
     linera_storage::WallClock,
 >;
 
-pub async fn get_storage() -> Result<WebStorage, <WebStorage as linera_storage::Storage>::ContextError> {
+pub async fn get_storage() -> Result<WebStorage, <WebStorage as linera_storage::Storage>::StoreError> {
     linera_storage::DbStorage::new(
         linera_views::memory::MemoryStoreConfig::new(1),
         "linera",
@@ -56,125 +35,56 @@ pub async fn get_storage() -> Result<WebStorage, <WebStorage as linera_storage::
     ).await
 }
 
-pub struct ClientContext {
-    wallet_state: WalletState,
-    chain_client_builder: ChainClientBuilder<NodeProvider>,
-    send_timeout: Duration,
-    recv_timeout: Duration,
-    notification_retry_delay: Duration,
-    notification_retries: u32,
-    prng: Box<dyn CryptoRng>,
-}
 
-impl ClientContext {
-    fn wallet_state(&self) -> &WalletState {
-        &self.wallet_state
-    }
+type PersistentWallet = linera_client::persistent::LocalStorage<Wallet>;
+type ClientContext = linera_client::client_context::ClientContext<WebStorage, PersistentWallet>;
 
-    fn make_chain_client<S>(&self, storage: S, chain_id: ChainId) -> ChainClient<NodeProvider, S> {
-        let chain = self
-            .wallet_state.0
-            .get(chain_id)
-            .unwrap_or_else(|| panic!("Unknown chain: {}", chain_id));
-        let known_key_pairs = chain
-            .key_pair
-            .as_ref()
-            .map(|kp| kp.copy())
-            .into_iter()
-            .collect();
-        self.chain_client_builder.build(
-            chain_id,
-            known_key_pairs,
-            storage,
-            self.wallet_state.0.genesis_admin_chain(),
-            chain.block_hash,
-            chain.timestamp,
-            chain.next_block_height,
-            chain.pending_block.clone(),
-            chain.pending_blobs.clone(),
-        )
-    }
+// TODO get from config
+pub const OPTIONS: ClientOptions = ClientOptions {
+    wallet_state_path: None,
+    storage_config: None,
+    with_wallet: None,
 
-    fn update_wallet_for_new_chain(
-        &mut self,
-        chain_id: ChainId,
-        key_pair: Option<KeyPair>,
-        timestamp: Timestamp,
-    ) {
-        if self.wallet_state.0.get(chain_id).is_none() {
-            self.wallet_state.0.insert(wallet::UserChain {
-                chain_id,
-                key_pair: key_pair.as_ref().map(|kp| kp.copy()),
-                block_hash: None,
-                timestamp,
-                next_block_height: BlockHeight::ZERO,
-                pending_block: None,
-                pending_blobs: BTreeMap::default(),
-            });
-        }
-    }
+    // Timeout for sending queries (milliseconds)
+    send_timeout: std::time::Duration::from_millis(4000),
+    recv_timeout: std::time::Duration::from_millis(4000),
+    max_pending_messages: 10,
 
-    pub async fn update_and_save_wallet<'a, S>(&mut self, state: &mut ChainClient<NodeProvider, S>)
-    where
-        S: linera_storage::Storage + Clone + Send + Sync + 'static,
-        linera_views::views::ViewError: From<S::ContextError>,
-    {
-        self.wallet_state.0.update_from_state(state).await;
-        self.wallet_state.0.refresh_prng_seed(&mut self.prng);
-        self.wallet_state.write();
-    }
+    // The WebAssembly runtime to use.
+    wasm_runtime: None,
 
+    // The maximal number of simultaneous queries to the database
+    max_concurrent_queries: None,
 
+    // The maximal number of simultaneous stream queries to the database
+    max_stream_queries: 10,
 
+    // The maximal number of entries in the storage cache.
+    cache_size: 1000,
 
-    pub fn make_node_provider(&self) -> NodeProvider {
-        NodeProvider::new(self.make_node_options())
-    }
+    // Delay increment for retrying to connect to a validator for notifications.
+    notification_retry_delay: std::time::Duration::from_millis(1000),
 
-    fn make_node_options(&self) -> NodeOptions {
-        NodeOptions {
-            send_timeout: self.send_timeout,
-            recv_timeout: self.recv_timeout,
-            notification_retry_delay: self.notification_retry_delay,
-            notification_retries: self.notification_retries,
-        }
-    }
-}
+    // Number of times to retry connecting to a validator for notifications.
+    notification_retries: 10,
+
+    // Whether to wait until a quorum of validators has confirmed that all sent cross-chain
+    // messages have been delivered.
+    wait_for_outgoing_messages: false,
+
+    // The policy for handling incoming messages.
+    message_policy: linera_core::client::MessagePolicy::Accept,
+
+    // A dummy command, for now
+    command: linera_client::client_options::ClientCommand::Keygen,
+
+    tokio_threads: Some(1),
+};
 
 pub async fn get_client_context(wallet: &Wallet) -> Result<ClientContext, JsError> {
-    // TODO get from config
-    let options: ClientOptions = options::OPTIONS;
-
-    Ok(ClientContext {
-        prng: wallet.make_prng(),
-        wallet_state: WalletState(wallet.copy()),
-        chain_client_builder: ChainClientBuilder::new(
-            NodeProvider::new(NodeOptions {
-                send_timeout: options.send_timeout,
-                recv_timeout: options.recv_timeout,
-                notification_retry_delay: options.notification_retry_delay,
-                notification_retries: options.notification_retries,
-            }),
-            options.max_pending_messages,
-            linera_core::node::CrossChainMessageDelivery::new(true),
-        ),
-        send_timeout: options.send_timeout,
-        recv_timeout: options.recv_timeout,
-        notification_retry_delay: options.notification_retry_delay,
-        notification_retries: options.notification_retries,
-    })
-}
-
-struct WalletState(Wallet);
-
-impl WalletState {
-    fn refresh_prng_seed(&mut self) {
-        // TODO
-    }
-
-    fn write(&self) {
-        // TODO
-    }
+    let mut storage = get_storage().await?;
+    wallet.genesis_config().initialize_storage(&mut storage).await?;
+    Ok(ClientContext::new(get_storage().await?, OPTIONS, OPTIONS.wallet()?))
 }
 
 #[wasm_bindgen]
@@ -190,7 +100,7 @@ pub async fn dapp_query_validators() -> Result<(), JsError> {
 
     let mut client_context: ClientContext = get_client_context(wallet).await?;
 
-    let mut chain_client = client_context.make_chain_client(storage, chain_id);
+    let mut chain_client = client_context.make_chain_client(chain_id);
     log::info!(
         "Querying the validators of the current epoch of chain {}",
         chain_id
@@ -240,7 +150,7 @@ pub async fn dapp_query(n: u32) -> u32 {
 pub async fn main() {
     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
 
-    console_log::init_with_level(log::Level::Debug);
+    console_log::init_with_level(log::Level::Debug).unwrap();
 
     log::info!("Hello World!");
 }
