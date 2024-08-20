78fc6228cd006bff4482e82f1eacea60a1e85768
James Kay <james.kay@linera.io>
2024-08-20 23:20:07 +0100

    `client-worker`: non-static API

--- client-worker/src/lib.rs
+++ client-worker/src/lib.rs
@@ -19,6 +19,9 @@ use web_sys::*;
 
 use linera_client::{chain_listener::ClientContext as _, client_options::ClientOptions, wallet::Wallet};
 
+use std::{collections::HashMap, sync::Arc};
+use futures::lock::Mutex as AsyncMutex;
+
 // TODO convert to IndexedDbStore once we refactor Context
 type WebStorage = linera_storage::DbStorage<
     linera_views::memory::MemoryStore,
@@ -39,100 +42,101 @@ type ClientContext = linera_client::client_context::ClientContext<WebStorage, Pe
 
 // TODO get from config
 pub const OPTIONS: ClientOptions = ClientOptions {
-    wallet_state_path: None,
-    storage_config: None,
-    with_wallet: None,
-
-    // Timeout for sending queries (milliseconds)
     send_timeout: std::time::Duration::from_millis(4000),
     recv_timeout: std::time::Duration::from_millis(4000),
     max_pending_messages: 10,
-
-    // The WebAssembly runtime to use.
     wasm_runtime: None,
-
-    // The maximal number of simultaneous queries to the database
     max_concurrent_queries: None,
-
-    // The maximal number of simultaneous stream queries to the database
     max_stream_queries: 10,
-
-    // The maximal number of entries in the storage cache.
     cache_size: 1000,
-
-    // Delay increment for retrying to connect to a validator for notifications.
     notification_retry_delay: std::time::Duration::from_millis(1000),
-
-    // Number of times to retry connecting to a validator for notifications.
     notification_retries: 10,
-
-    // Whether to wait until a quorum of validators has confirmed that all sent cross-chain
-    // messages have been delivered.
     wait_for_outgoing_messages: false,
-
-    // The policy for handling incoming messages.
     message_policy: linera_core::client::MessagePolicy::Accept,
 
-    // A dummy command, for now
-    command: linera_client::client_options::ClientCommand::Keygen,
-
+    // TODO: separate these out from the `ClientOptions` struct, since
+    // the apply only to the CLI/native client
     tokio_threads: Some(1),
+    command: linera_client::client_options::ClientCommand::Keygen,
+    wallet_state_path: None,
+    storage_config: None,
+    with_wallet: None,
 };
 
-pub async fn get_client_context() -> Result<ClientContext, JsError> {
-    let wallet = linera_client::config::WalletState::read_from_local_storage("linera-wallet")?;
-    let mut storage = get_storage().await?;
-    wallet.genesis_config().initialize_storage(&mut storage).await?;
-    Ok(ClientContext::new(get_storage().await?, OPTIONS, wallet))
-}
+/// The full client API, exposed to the wallet implementation.  Calls
+/// to this API can be trusted to have originated from the user's
+/// request.
+#[wasm_bindgen]
+pub struct Client(
+    // This use of `futures::lock::Mutex` is safe because we only
+    // expose concurrency to the browser, which must always run all
+    // futures on the global task queue.
+    Arc<AsyncMutex<ClientContext>>,
+);
+
+/// The subset of the client API that should be exposed to application
+/// frontends.  The API here is directly exposed to random Web pages
+/// on the Internet, and so calls should not be trusted.
+#[wasm_bindgen]
+pub struct Frontend(Arc<AsyncMutex<ClientContext>>);
 
 #[wasm_bindgen]
-pub async fn dapp_query_validators() -> Result<(), JsError> {
-    let mut client_context: ClientContext = get_client_context().await?;
-    let chain_id = client_context.wallet().default_chain().expect("No default chain");
-
-    let mut chain_client = client_context.make_chain_client(chain_id);
-    log::info!(
-        "Querying the validators of the current epoch of chain {}",
-        chain_id
-    );
-    chain_client.synchronize_from_validators().await?;
-    log::info!("Synchronized state from validators");
-    let result = chain_client.local_committee().await;
-    client_context.update_and_save_wallet(&mut chain_client).await;
-    let committee = result?;
-    log::info!("{:?}", committee.validators());
-    let node_provider = client_context.make_node_provider();
-    for (name, state) in committee.validators() {
-        match node_provider
-            .make_node(&state.network_address)?
-            .get_version_info()
-            .await
-        {
-            Ok(version_info) => {
-                log::info!(
-                    "Version information for validator {name:?}:{}",
-                    version_info
-                );
-            }
-            Err(e) => {
-                log::warn!("Failed to get version information for validator {name:?}:\n{e}")
-            }
-        }
+impl Client {
+    #[wasm_bindgen(constructor)]
+    pub async fn new() -> Result<Client, JsError> {
+        let wallet = linera_client::config::WalletState::read_from_local_storage("linera-wallet")?;
+        let mut storage = get_storage().await?;
+        wallet.genesis_config().initialize_storage(&mut storage).await?;
+        let storage = get_storage().await?;
+        log::info!("Linera Web client successfully initialized");
+        Ok(Self(Arc::new(futures::lock::Mutex::new(ClientContext::new(storage, OPTIONS, wallet)))))
     }
 
-    Ok(())
+    #[wasm_bindgen]
+    pub fn frontend(&self) -> Frontend {
+        Frontend(self.0.clone())
+    }
 }
 
 #[wasm_bindgen]
-pub async fn set_wallet(wallet: &str) -> Result<(), wasm_bindgen::JsError> {
-    linera_client::config::WalletState::create_from_local_storage("linera-wallet", serde_json::from_str(wallet)?)?;
-    Ok(())
-}
+impl Frontend {
+    #[wasm_bindgen]
+    pub async fn validator_version_info(&self) -> Result<JsValue, JsError> {
+        let mut client_context = self.0.lock().await;
+        let chain_id = client_context.wallet().default_chain().expect("No default chain");
+        let mut chain_client = client_context.make_chain_client(chain_id);
 
-#[wasm_bindgen]
-pub async fn dapp_query(n: u32) -> u32 {
-    n + 1
+        log::info!(
+            "Querying the validators of the current epoch of chain {}",
+            chain_id
+        );
+        chain_client.synchronize_from_validators().await?;
+        log::info!("Synchronized state from validators");
+        let result = chain_client.local_committee().await;
+        client_context.update_and_save_wallet(&mut chain_client).await;
+        let committee = result?;
+        log::info!("{:?}", committee.validators());
+        let node_provider = client_context.make_node_provider();
+
+        let mut validator_versions = HashMap::new();
+
+        for (name, state) in committee.validators() {
+            match node_provider
+                .make_node(&state.network_address)?
+                .get_version_info()
+                .await
+            {
+                Ok(version_info) => if validator_versions.insert(name, version_info).is_some() {
+                    log::warn!("duplicate validator entry for validator {name:?}");
+                }
+                Err(e) => {
+                    log::warn!("failed to get version information for validator {name:?}:\n{e:?}")
+                }
+            }
+        }
+
+        Ok(serde_wasm_bindgen::to_value(&validator_versions)?)
+    }
 }
 
 #[wasm_bindgen(start)]
@@ -141,5 +145,5 @@ pub async fn main() {
 
     console_log::init_with_level(log::Level::Debug).unwrap();
 
-    log::info!("Hello World!");
+    log::info!("Linera Web client loaded");
 }
