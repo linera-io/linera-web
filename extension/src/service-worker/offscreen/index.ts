026888cd0c2682425abfda69e433a9fbb331f7cb
James Kay <james.kay@linera.io>
2024-11-25 22:57:32 +0000

    Remove dead code

--- extension/src/service-worker/offscreen/index.ts
+++ extension/src/service-worker/offscreen/index.ts
@@ -4,8 +4,9 @@ let creating: Promise<void> | null; // A global promise to avoid concurrency iss
 
 export async function setup() {
   console.log('Creating offscreen document');
-  // TODO this is a bit of a hack: I'd like to import the URL of the
-  // processed HTML instead of making it a root
+  // This is a bit of a hack: I'd like to import the URL of the
+  // processed HTML instead of making it a root, but I can't find a
+  // way to do so in Vite.
   const offscreenUrl = chrome.runtime.getURL('src/service-worker/offscreen/index.html');
 
   // Check all windows controlled by the service worker to see if one
@@ -20,7 +21,6 @@ export async function setup() {
     return;
   }
 
-  // create offscreen document
   if (!creating)
     creating = chrome.offscreen.createDocument({
       url: offscreenUrl,
