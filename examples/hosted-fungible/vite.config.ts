b3fafa5ccf5c6494650e276cfe4a114761eca3a4
James Kay <james.kay@linera.io>
2025-03-27 12:18:48 +0000

    Hosted fungible example (#39)
    * Hosted fungible example
    
    * `examples/hosted-counter`: fix up style for shared stylesheet
    
    * `hosted-fungible`: improve error handling
    
    * `examples/hosted-fungible`: add copy button
    
    * `client`: update manifest for publishing
    
    * `linera-protocol`: bring to trunk

---
+++ examples/hosted-fungible/vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/hosted/fungible/',
+  server: {
+    headers: {
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+      'Cross-Origin-Opener-Policy': 'same-origin',
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        index: 'index.html',
+        linera: '@linera/client',
+      },
+      preserveEntrySignatures: 'strict',
+    },
+  },
+  esbuild: {
+    supported: {
+      'top-level-await': true,
+    },
+  },
+  optimizeDeps: {
+    exclude: [
+      '@linera/client',
+    ],
+  },
+})
