926a2a56f15a1bd8fdf95e2f826cdda947c8b575
James Kay <james.kay@linera.io>
2025-07-03 15:43:11 +0100

    Environment symlinks (#54)
    * Add symlinks to .env in examples
    
    * `examples`: use relative paths for easier deployment

--- examples/hosted-fungible/vite.config.ts
+++ examples/hosted-fungible/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/hosted/fungible/',
+  base: '',
   server: {
     headers: {
       'Cross-Origin-Embedder-Policy': 'require-corp',
